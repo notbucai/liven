43ce175eacd265ad0bc1c9439cd1bc1c4c56160f
bucai <1450941858@qq.com>
2020-01-01 21:25:51 +0800

    修改组件呈现方式

--- frontend/src/components/pinItem/Layout.tsx
+++ frontend/src/components/pinItem/Layout.tsx
@@ -4,6 +4,20 @@ import User from './components/user';
 import Image from './components/image';
 
 const Layout = () => {
+  const picList = [
+    {
+      "createTime": "2019-12-04T06:48:34.755Z",
+      "_id": "5de756eb31a32073dbe67a5d",
+      "user": "5de097b2f966bb59c0ee195f",
+      "url": "7cf307e94b8f86e25e9197ef0032934b",
+    },
+    {
+      "createTime": "2019-12-04T06:48:34.755Z",
+      "_id": "5de756eb31a32073dbe67a5d",
+      "user": "5de097b2f966bb59c0ee195f",
+      "url": "7cf307e94b8f86e25e9197ef0032934b",
+    },
+  ];
   return (
     <div className={styles.pin}>
       <User />
@@ -18,7 +32,7 @@ s这个需求今晚要上线！！！`
         </span>
       </div>
       <div className={styles.main}>
-        <Image list={[]}/>
+        <Image list={[]} />
         <div className={styles.tag}>
           <a href="1">掘金相亲</a>
         </div>
